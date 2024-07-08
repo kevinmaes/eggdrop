f92e257cf51b5263f4d3057d696c7737d3e95fa3
Kevin Maes <kevin@kevinmaes.com>
2024-07-08 16:26:55 +0200

    Add keyboard functionality to move the Chef

--- src/Chef/Chef.tsx
+++ src/Chef/Chef.tsx
@@ -1,32 +1,93 @@
-import { useEffect } from 'react';
-import { useMachine } from '@xstate/react';
-import { Image as KonvaImage } from 'react-konva';
-import useImage from 'use-image';
-import { chefMachine } from './chef.machine';
+import { useEffect, useState } from 'react';
+import { Image as KonvaImage, Rect } from 'react-konva';
+// import useImage from 'use-image';
 
 export function Chef() {
-	const [current, send] = useMachine(chefMachine);
-	const [chefImage] = useImage('path-to-your-chef-image.png');
+	// const [chefImage] = useImage('path-to-your-chef-image.png');
 
 	useEffect(() => {
 		const handleKeyDown = (e: KeyboardEvent) => {
 			if (e.key === 'ArrowLeft') {
-				send({ type: 'MOVE_LEFT' });
+				setXDirection(-1);
 			} else if (e.key === 'ArrowRight') {
-				send({ type: 'MOVE_RIGHT' });
+				setXDirection(1);
+			}
+		};
+		const handleKeyUp = (e: KeyboardEvent) => {
+			if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+				setXDirection(0);
 			}
 		};
 		window.addEventListener('keydown', handleKeyDown);
-		return () => window.removeEventListener('keydown', handleKeyDown);
-	}, [send]);
+		window.addEventListener('keyup', handleKeyUp);
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+			window.removeEventListener('keyup', handleKeyUp);
+		};
+	}, []);
 
-	return (
-		<KonvaImage
-			image={chefImage}
-			x={current.context.position.x}
-			y={current.context.position.y}
-			width={50}
-			height={50}
-		/>
-	);
+	const leftXLimit = 0;
+	const rightXLimit = window.innerWidth - 75;
+	const speedLimit = 20;
+	const accel = 0.1 * speedLimit;
+	const decel = 1;
+
+	const [xPos, setXPos] = useState(window.innerWidth / 2);
+	const [yPos] = useState(window.innerHeight - 120);
+	const [xDirection, setXDirection] = useState(0);
+	const [speed, setSpeed] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			let newSpeed = speed;
+
+			let newXPos = xPos;
+
+			if (xDirection === 0) {
+				if (speed > 0) {
+					newSpeed = Math.max(speed - decel, 0);
+					if (newSpeed > speedLimit) {
+						newSpeed = speedLimit;
+					}
+				} else if (speed < 0) {
+					newSpeed = Math.min(speed + decel, 0);
+					if (Math.abs(newSpeed) > speedLimit) {
+						newSpeed = -speedLimit;
+					}
+				}
+			} else {
+				if (xDirection) {
+					newSpeed = speed + xDirection * accel;
+				}
+			}
+
+			newXPos = xPos + newSpeed;
+
+			if (newXPos < leftXLimit) {
+				newXPos = leftXLimit;
+				newSpeed = 0;
+			} else if (newXPos > rightXLimit) {
+				newXPos = rightXLimit;
+				newSpeed = 0;
+			}
+
+			setSpeed(newSpeed);
+			setXPos(newXPos);
+		}, 16);
+
+		return () => clearInterval(interval);
+	});
+
+	// Render a square for now
+	return <Rect x={xPos} y={yPos} width={75} height={100} fill="green" />;
+
+	// return (
+	// 	<KonvaImage
+	// 		image={chefImage}
+	// 		x={state.context.position.x}
+	// 		y={state.context.position.y}
+	// 		width={50}
+	// 		height={50}
+	// 	/>
+	// );
 }
