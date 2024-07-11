8bdb403541f1ea74c95b4714d44aa797bc0bddc4
Kevin Maes <kevin@kevinmaes.com>
2024-07-11 22:44:43 +0200

    Clean up anim object by setting to null

--- src/helpers/animationActor.ts
+++ src/helpers/animationActor.ts
@@ -2,11 +2,13 @@ import { Animation } from 'konva/lib/Animation';
 import { fromPromise } from 'xstate';
 
 export const animationActor = fromPromise(() => {
+	let anim: Animation | null;
 	return new Promise<{ timeDiff: number }>((resolve) => {
-		const anim = new Animation((frame) => {
+		anim = new Animation((frame) => {
 			if (frame?.timeDiff) {
 				resolve({ timeDiff: frame?.timeDiff });
-				anim.stop();
+				anim?.stop();
+				anim = null;
 			}
 		});
 		anim.start();
