7501e9ed8ffdf1a1335acdc60c2a3f1b14c9acdb
Kevin Maes <kevin@kevinmaes.com>
2024-07-15 22:59:15 +0200

    Increase level time

--- src/GameLevel/gameConfig.ts
+++ src/GameLevel/gameConfig.ts
@@ -1,4 +1,4 @@
-export const LEVEL_DURATION_MS = 10_000;
+export const LEVEL_DURATION_MS = 30_000;
 export const POPULATION_SIZE = 10;
 export const STAGE_DIMENSIONS = { width: 1920, height: 1080 };
 export const CHEF_DIMENSIONS = { width: 224, height: 150 };
