bdb31243b7a6ac856cb3ae1be0e4b8fed00de942
Kevin Maes <kevin@kevinmaes.com>
2024-07-28 21:11:04 +0200

    Clean up

---
+++ src/Egg/animation.ts
@@ -0,0 +1,34 @@
+import Konva from 'konva';
+import { AnyActorRef, fromPromise } from 'xstate';
+
+type AnimationProps = {
+	duration: number;
+	x: number;
+	y: number;
+	rotation: number;
+	onUpdate?: () => void;
+	onFinish: () => void;
+};
+
+export const animationActor = fromPromise(
+	({
+		input,
+	}: {
+		input: {
+			id: string;
+			ref: React.RefObject<Konva.Rect>;
+			parentRef: AnyActorRef;
+			animationProps: AnimationProps;
+		};
+	}) => {
+		return new Promise<void>((resolve) => {
+			console.log('Running animation promise');
+			input.ref.current?.to({
+				...input.animationProps,
+				onFinish: () => {
+					resolve();
+				},
+			});
+		});
+	}
+);
