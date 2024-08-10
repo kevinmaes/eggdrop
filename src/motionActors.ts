c0cf35e887f1f66f805c28b5fe1bad4e7dfade51
Kevin Maes <kevin@kevinmaes.com>
2024-08-10 18:27:33 +0200

    Add docs

--- src/motionActors.ts
+++ src/motionActors.ts
@@ -2,6 +2,11 @@ import Konva from 'konva';
 import { fromPromise } from 'xstate';
 import { Position } from './GameLevel/types';
 
+/**
+ * Takes any node and its pre-existing tween and plays the tween
+ * Any tween callbacks may have already been attached to the tween
+ * outside of this actor
+ */
 export const tweenActor = fromPromise<
 	{ endPosition: Position },
 	{
