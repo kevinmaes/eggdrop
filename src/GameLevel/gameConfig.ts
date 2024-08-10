534546242abd30ba15dfe2b83a0f44c230c9c898
Kevin Maes <kevin@kevinmaes.com>
2024-08-10 18:16:35 +0200

    Tweak some config settings

--- src/GameLevel/gameConfig.ts
+++ src/GameLevel/gameConfig.ts
@@ -1,5 +1,5 @@
 export const LEVEL_DURATION_MS = 30_000;
-export const POPULATION_SIZE = 1;
+export const POPULATION_SIZE = 10;
 export const STAGE_DIMENSIONS = { width: 1920, height: 1080 };
 export const CHEF_DIMENSIONS = { width: 224, height: 150 };
 export const CHEF_POT_RIM_CONFIG = {
@@ -13,22 +13,22 @@ export const STAGGERED_HEN_DELAY_MS = 1000;
 
 export function getInitialChromosomeValues() {
 	const minX = Math.random() * 0.4 * STAGE_DIMENSIONS.width;
-	// const minX = 0.1 * STAGE_DIMENSIONS.width;
 	const maxX = STAGE_DIMENSIONS.width - minX;
 	const minStopMS = 1000; // Math.ceil(Math.random() * 1000);
 	const maxStopMS = 1000; // minStopMS + Math.random() * 5000;
 
 	return {
 		// speed: Math.random() * 1.2,
-		speed: Math.random() * 1.2 + 0.5,
+		speed: Math.random() * 2, // + 0.5,
 		// speed: 2,
 		baseTweenDurationSeconds: Math.ceil(Math.random() * 10),
 		maxEggs: -1,
-		// stationaryEggLayingRate: Math.random(),
-		stationaryEggLayingRate: 0,
-		// movingEggLayingRate: Math.random(),
-		movingEggLayingRate: 1,
-		hatchRate: 1, // Math.random(),
+		stationaryEggLayingRate: Math.random(),
+		// stationaryEggLayingRate: 0,
+		movingEggLayingRate: Math.random(), // * 0.5,
+		// movingEggLayingRate: 1,
+		hatchRate: Math.random(),
+		// hatchRate: 1,
 		minX,
 		maxX,
 		minStopMS,
