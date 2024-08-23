0c7952858be2521c08bef4b37bbfee61f5dad144
Kevin Maes <kevin@kevinmaes.com>
2024-08-23 22:39:39 +0200

    Animate hens moving side to side

--- src/Hen/Hen.tsx
+++ src/Hen/Hen.tsx
@@ -4,7 +4,7 @@ import useImage from 'use-image';
 import { useSelector } from '@xstate/react';
 import { ActorRefFrom } from 'xstate';
 import { henMachine } from './hen.machine';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Konva from 'konva';
 
 export function Hen({
@@ -12,18 +12,22 @@ export function Hen({
 }: {
 	henActorRef: ActorRefFrom<typeof henMachine>;
 }) {
-	const { henFrames, position, isLaying, direction, tweenSpeed } = useSelector(
-		henActorRef,
-		(state) => ({
-			henFrames: state.context.henAssets.sprite.frames,
-			henFrameNames: Object.keys(state.context.henAssets.sprite.frames),
-			position: state.context.position,
-			isMoving: state.matches('Moving'),
-			isLaying: state.matches('Laying Egg'),
-			direction: state.context.currentTweenDirection,
-			tweenSpeed: state.context.currentTweenSpeed,
-		})
-	);
+	const {
+		henFrames,
+		position,
+		isLaying,
+		isMoving,
+		direction,
+		absoluteTweenSpeed,
+	} = useSelector(henActorRef, (state) => ({
+		henFrames: state.context.henAssets.sprite.frames,
+		henFrameNames: Object.keys(state.context.henAssets.sprite.frames),
+		position: state.context.position,
+		isMoving: state.matches('Moving'),
+		isLaying: state.matches('Laying Egg'),
+		direction: state.context.currentTweenDirection,
+		absoluteTweenSpeed: Math.abs(state.context.currentTweenSpeed),
+	}));
 	// console.log('direction', direction);
 	const [image] = useImage('images/hen.sprite.png');
 
@@ -38,29 +42,68 @@ export function Hen({
 		return null;
 	}
 
-	let frameName = '1-hen-front.png';
-	switch (true) {
-		case isLaying:
-			// Randomly choose laying frame
-			const layingFrameNames = [
-				'3-hen-back.png',
-				'4-hen-jump1.png',
-				'5-hen-jump2.png',
-			];
-			frameName =
-				layingFrameNames[Math.floor(Math.random() * layingFrameNames.length)];
-			break;
-		case direction === -1:
-			frameName = 'hen-walk-left1.png';
-			// frameName = 'hen-walk-left2.png';
-			break;
-		case direction === 1:
-			frameName = 'hen-walk-right1.png';
-			// frameName = 'hen-walk-right2.png';
-			break;
-		default:
-			frameName = '1-hen-front.png';
-	}
+	const [frameName, setFrameName] = useState('1-hen-front.png');
+	useEffect(() => {
+		let interval: ReturnType<typeof setInterval> | null = null;
+		const animationIntervalMSRange = [50, 1000];
+
+		switch (true) {
+			case isLaying: {
+				// Randomly choose laying frame
+				const layingFrameNames = [
+					'3-hen-back.png',
+					'4-hen-jump1.png',
+					'5-hen-jump2.png',
+				];
+				// frameName =
+				setFrameName(
+					layingFrameNames[Math.floor(Math.random() * layingFrameNames.length)]
+				);
+				break;
+			}
+			case isMoving && direction === -1: {
+				// Calculate intervalMS based on tweenSpeed where higher tweenSpeed results
+				// in a lower intervalMS within the animationIntervalMSRange
+				const intervalMS = Math.max(
+					animationIntervalMSRange[0],
+					animationIntervalMSRange[1] - absoluteTweenSpeed * 100
+				);
+				interval = setInterval(() => {
+					setFrameName((prevFrameName) =>
+						prevFrameName === 'hen-walk-left1.png'
+							? 'hen-walk-left2.png'
+							: 'hen-walk-left1.png'
+					);
+				}, intervalMS);
+				break;
+			}
+			case isMoving && direction === 1: {
+				// Calculate intervalMS based on tweenSpeed where higher tweenSpeed results
+				// in a lower intervalMS within the animationIntervalMSRange
+				const intervalMS = Math.max(
+					animationIntervalMSRange[0],
+					animationIntervalMSRange[1] - absoluteTweenSpeed * 100
+				);
+				interval = setInterval(() => {
+					setFrameName((prevFrameName) =>
+						prevFrameName === 'hen-walk-right1.png'
+							? 'hen-walk-right2.png'
+							: 'hen-walk-right1.png'
+					);
+				}, intervalMS);
+				break;
+			}
+			default: {
+				setFrameName('1-hen-front.png');
+			}
+		}
+
+		return () => {
+			if (interval) {
+				clearInterval(interval);
+			}
+		};
+	}, [isLaying, direction, isMoving, absoluteTweenSpeed]);
 
 	let currentFrame = henFrames[frameName].frame;
 
