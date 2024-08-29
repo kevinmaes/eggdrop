5114e3530268f02333378c35ff387d624864175e
Kevin Maes <kevin@kevinmaes.com>
2024-08-29 09:20:07 +0200

    Increase level time

--- src/GameLevel/gameConfig.ts
+++ src/GameLevel/gameConfig.ts
@@ -21,7 +21,7 @@ export function getGameConfig() {
 		// The number of hens in the game
 		populationSize: 10,
 		// The duration each level lasts in milliseconds
-		levelDurationMS: 5_000,
+		levelDurationMS: 50_000,
 		stageDimensions: {
 			...stageDimensions,
 			midX: stageDimensions.width / 2,
