d54fc2e5d56ea256a18bc7a50916f79ec9ef9d37
Kevin Maes <kevin@kevinmaes.com>
2024-08-30 15:40:31 +0200

    Wire up mute and sound buttons

--- src/MuteButton/MuteButton.tsx
+++ src/MuteButton/MuteButton.tsx
@@ -1,15 +1,25 @@
-import { Group, Rect } from 'react-konva';
+import { Group, Rect, Image } from 'react-konva';
 import { AppActorContext } from '../app.machine';
+import useImage from 'use-image';
 
 export function MuteButton() {
 	const appActorRef = AppActorContext.useActorRef();
-	const { gameConfig, isMuted } = AppActorContext.useSelector((state) => ({
-		gameConfig: state.context.gameConfig,
-		isMuted: state.context.isMuted,
-	}));
+	const { gameConfig, controlsFrames, isMuted } = AppActorContext.useSelector(
+		(state) => ({
+			gameConfig: state.context.gameConfig,
+			controlsFrames: state.context.gameAssets?.controls.frames ?? {},
+			isMuted: state.context.isMuted,
+		})
+	);
+
+	const [controlsImage] = useImage('images/controls.sprite.png');
+	const controlsFrame = isMuted
+		? controlsFrames['sound-off.png']?.frame
+		: controlsFrames['sound-on.png'].frame;
 
 	return (
 		<Group x={10} y={gameConfig.henBeam.y + gameConfig.henBeam.height + 10}>
+			{/* Border box */}
 			<Rect
 				stroke="white"
 				x={0}
@@ -19,6 +29,7 @@ export function MuteButton() {
 				strokeWidth={2}
 				cornerRadius={8}
 			/>
+			{/* Internal background box */}
 			<Rect
 				x={5}
 				y={5}
@@ -30,6 +41,20 @@ export function MuteButton() {
 				}}
 				fill="white"
 				opacity={isMuted ? 0.7 : 0.3}
+			/>
+			<Image
+				listening={false}
+				x={5}
+				y={5}
+				image={controlsImage}
+				width={40}
+				height={40}
+				crop={{
+					x: controlsFrame.x,
+					y: controlsFrame.y,
+					width: controlsFrame.w,
+					height: controlsFrame.h,
+				}}
 			/>
 		</Group>
 	);
