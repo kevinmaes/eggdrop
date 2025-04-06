e2e10334ae926b9df68b068b14eaa5778d1c3a1a
Kevin Maes <kevin@kevinmaes.com>
2025-04-07 00:25:26 +0700

    Force another ci error

--- src/Hen/Hen.tsx
+++ src/Hen/Hen.tsx
@@ -43,7 +43,7 @@ export function Hen({
 		},
 		henFrames: state.context.henAssets.frames,
 		position: state.context.position,
-		isMoving: state.matches('Movingx'),
+		isMoving: state.matches('Movingxx'),
 		isLaying: state.matches('Laying Egg'),
 		movingDirection: state.context.movingDirection,
 		absoluteTweenSpeed: Math.abs(state.context.currentTweenSpeed),
