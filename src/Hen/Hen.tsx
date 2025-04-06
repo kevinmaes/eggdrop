fc95348778d23b593d7b70d1a66d09dc00f4cd61
Kevin Maes <kevin@kevinmaes.com>
2025-04-07 00:28:56 +0700

    Fix ci errors

--- src/Hen/Hen.tsx
+++ src/Hen/Hen.tsx
@@ -43,7 +43,7 @@ export function Hen({
 		},
 		henFrames: state.context.henAssets.frames,
 		position: state.context.position,
-		isMoving: state.matches('Movingxx'),
+		isMoving: state.matches('Moving'),
 		isLaying: state.matches('Laying Egg'),
 		movingDirection: state.context.movingDirection,
 		absoluteTweenSpeed: Math.abs(state.context.currentTweenSpeed),
