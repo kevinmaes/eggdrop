f99179f894d4e1745c1d4b400b358a9357e17c71
Kevin Maes <kevin@kevinmaes.com>
2025-05-21 13:57:32 +0200

    Increase timeout to 20 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* Set global test timeout to 20 seconds */
+  timeout: 20000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
