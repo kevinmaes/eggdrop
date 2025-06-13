4c0105e73b2ac5c63e75cbe2ca2013d837d6087e
Kevin Maes <kevin@kevinmaes.com>
2025-06-13 19:41:14 +0200

    Add lightmode bg color

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const STAGE_HEIGHT = 563;
 export const BORDER_COLOR = '#EEE';
 export const BACKGROUND_COLOR = '#FFF';
 export const BACKGROUND_GREENSCREEN_COLOR = '#1DB100';
+export const BACKGROUND_STATELY_LIGHTMODE_COLOR = '#F7F7F7';
+
