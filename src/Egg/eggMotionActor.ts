a19b4045bf253dd8deb1a9608f1019c336e42aa0
Kevin Maes <kevin@kevinmaes.com>
2025-06-15 11:38:34 +0200

    Declare separate actor input interface

--- src/Egg/eggMotionActor.ts
+++ src/Egg/eggMotionActor.ts
@@ -3,75 +3,76 @@ import { type AnyActorRef, fromPromise } from 'xstate';
 
 import type { Direction, Position } from '../types';
 
-export const eggMotionActor = fromPromise<
-  Position,
-  {
-    node: React.RefObject<any>['current'];
-    initialPosition: Position;
-    xSpeed: number;
-    ySpeed: number;
-    testForDestination: (yPos: number) => boolean;
-    rotationDirection: Direction['value'];
-    parent: AnyActorRef;
-  }
->(({ input }) => {
-  return new Promise((resolve, reject) => {
-    if (!input.node) {
-      return reject('No eggRef');
-    }
+interface EggMotionActorInput {
+  node: React.RefObject<any>['current'];
+  initialPosition: Position;
+  xSpeed: number;
+  ySpeed: number;
+  testForDestination: (yPos: number) => boolean;
+  rotationDirection: Direction['value'];
+  parent: AnyActorRef;
+}
 
-    // Create new Animation instance
-    const animation = new Konva.Animation(frame => {
+export const eggMotionActor = fromPromise<Position, EggMotionActorInput>(
+  ({ input }) => {
+    return new Promise((resolve, reject) => {
       if (!input.node) {
-        animation.stop();
         return reject('No eggRef');
       }
 
-      // Check if the egg has reached the destination
-      const eggReachedDestination = input.testForDestination(input.node.y());
-      if (eggReachedDestination) {
-        animation.stop();
-        resolve({
-          x: input.node.x(),
-          y: input.node.y(),
-        });
-      }
+      // Create new Animation instance
+      const animation = new Konva.Animation(frame => {
+        if (!input.node) {
+          animation.stop();
+          return reject('No eggRef');
+        }
 
-      if (frame) {
-        // Calculate and set a new X position
-        const newXPos = input.node.x() + input.xSpeed;
-        input.node.x(newXPos);
-
-        // Calculate new y position with a minimum change threshold
-        const minYChange = 2.5; // Minimum change in Y position to prevent it from stalling
-        const deltaY = input.ySpeed * (frame.timeDiff / 1000);
-
-        // Ensure there's always a minimum change in the Y position
-        const newYPos =
-          input.node.y() +
-          (Math.abs(deltaY) > minYChange
-            ? deltaY
-            : minYChange * Math.sign(input.ySpeed));
-        // Set the new Y position
-        input.node.y(newYPos);
-
-        // Rotate the egg a bit more in whatever direction it's already rotating
-        const currentRotation = input.node.rotation();
-        const newRotation = currentRotation + input.rotationDirection * 5;
-        input.node.rotation(newRotation);
-
-        // Send a message to the parent to update the egg position
-        // Make sure the egg actor ref is still active
-        if (input.parent.getSnapshot().status === 'active') {
-          input.parent.send({
-            type: 'Notify of animation position',
-            position: { x: newXPos, y: newYPos },
+        // Check if the egg has reached the destination
+        const eggReachedDestination = input.testForDestination(input.node.y());
+        if (eggReachedDestination) {
+          animation.stop();
+          resolve({
+            x: input.node.x(),
+            y: input.node.y(),
           });
         }
-      }
-    });
 
-    // Start the animation immediately
-    animation.start();
-  });
-});
+        if (frame) {
+          // Calculate and set a new X position
+          const newXPos = input.node.x() + input.xSpeed;
+          input.node.x(newXPos);
+
+          // Calculate new y position with a minimum change threshold
+          const minYChange = 2.5; // Minimum change in Y position to prevent it from stalling
+          const deltaY = input.ySpeed * (frame.timeDiff / 1000);
+
+          // Ensure there's always a minimum change in the Y position
+          const newYPos =
+            input.node.y() +
+            (Math.abs(deltaY) > minYChange
+              ? deltaY
+              : minYChange * Math.sign(input.ySpeed));
+          // Set the new Y position
+          input.node.y(newYPos);
+
+          // Rotate the egg a bit more in whatever direction it's already rotating
+          const currentRotation = input.node.rotation();
+          const newRotation = currentRotation + input.rotationDirection * 5;
+          input.node.rotation(newRotation);
+
+          // Send a message to the parent to update the egg position
+          // Make sure the egg actor ref is still active
+          if (input.parent.getSnapshot().status === 'active') {
+            input.parent.send({
+              type: 'Notify of animation position',
+              position: { x: newXPos, y: newYPos },
+            });
+          }
+        }
+      });
+
+      // Start the animation immediately
+      animation.start();
+    });
+  }
+);
