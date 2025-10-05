9500bd29c6dfe0ef116da40a05d20319f45cd546
Kevin Maes <kevin@kevinmaes.com>
2025-10-05 11:47:09 +0200

    Update dependencies (#141)
    * Update XState
    
    * Update dependencies
    
    * Migrate to pnpm
    
    * Install pnpm before setup-node in CI
    
    * Update badges
    
    * Remove explicit version of pnpm from the ci workflows
    
    * Fix __dirname
    
    * Use node v22
    
    * Use pnpm for playwright ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'pnpm dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
