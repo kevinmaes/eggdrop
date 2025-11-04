80df5c66919d02a7036b41ae38ef8585675d0f09
Kevin Maes <kevin@kevinmaes.com>
2025-11-04 14:14:37 +0100

    Increase allowable chunk size for now

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase to 1MB
+  },
 });
