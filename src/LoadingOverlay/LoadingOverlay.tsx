84afa50c0333041d8d352bd539ad8ea8a39084ae
Kevin Maes <kevin@kevinmaes.com>
2025-11-10 10:01:18 +0100

    chore: inline loader svg assets for faster paint

--- src/LoadingOverlay/LoadingOverlay.tsx
+++ src/LoadingOverlay/LoadingOverlay.tsx
@@ -1,7 +1,25 @@
 import { useEffect, useId, useMemo, useState } from 'react';
 
-import eggFillSrc from '../assets/svg/egg-gold-flat.svg';
-import eggShellSrc from '../assets/svg/egg-white-flat.svg';
+const EGG_SHELL_SVG = `
+<svg width="120" height="160" viewBox="0 0 30 40" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <radialGradient id="egg-shell-gradient" cx="12" cy="15" r="53" fx="12" fy="15" gradientUnits="userSpaceOnUse">
+      <stop offset="0%" stop-color="#ffffff" stop-opacity="1"></stop>
+      <stop offset="100%" stop-color="#ff007f" stop-opacity="1"></stop>
+    </radialGradient>
+  </defs>
+  <g transform="scale(1,-1) translate(0,-40)">
+    <path d="M 11.572032 30.626894 C 13.195086 30.626894 14.472609 30.186552 15.768286 29.488800 C 18.873883 27.816370 21.719542 22.041264 22.557304 18.689818 C 22.807578 17.688634 23.581721 14.210572 23.300863 13.367922 C 23.223213 13.134928 22.881550 12.909206 22.693091 12.585482 C 22.862431 12.698388 22.963490 12.868238 23.093963 12.824738 C 23.532571 12.678523 23.015411 9.847272 22.867662 9.403989 C 22.043338 6.930727 20.948618 4.964925 18.897709 3.286715 C 15.878748 0.816385 10.975051 0.235203 7.518021 2.096894 C 6.826283 2.469405 6.089278 2.848610 5.474844 3.351380 C 5.154935 3.613158 4.872470 4.069692 4.395070 4.308418 C 3.388376 5.270205 2.678400 6.493044 2.170864 7.677440 C -0.134425 13.057038 1.238891 19.134327 3.910140 24.095774 C 4.788476 25.727153 5.879988 27.430077 7.336978 28.622293 C 8.353246 29.453878 9.410161 29.998876 10.653901 30.413498 C 10.895615 30.494083 11.176848 30.468803 11.572032 30.626894 Z" fill="url(#egg-shell-gradient)" transform="translate(2.8,-0.8)"></path>
+  </g>
+</svg>
+`;
+
+const EGG_FILL_GRADIENT = `
+<linearGradient id="egg-fill-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+  <stop offset="0%" stop-color="#ffff07"></stop>
+  <stop offset="100%" stop-color="#ff007f"></stop>
+</linearGradient>
+`;
 
 import type { LoadingStatus } from '../Loading/loading.machine';
 import './LoadingOverlay.css';
@@ -161,12 +179,34 @@ export function LoadingOverlay({ status }: LoadingOverlayProps) {
               }}
             >
               <defs>
+                <g id="egg-shell-vector">
+                  <svg
+                    width="120"
+                    height="160"
+                    viewBox="0 0 30 40"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <defs dangerouslySetInnerHTML={{ __html: EGG_FILL_GRADIENT }} />
+                  </svg>
+                </g>
+                <rect id="egg-fill-rect" width={SVG_WIDTH} height={SVG_HEIGHT} />
+                <g id={clipPathId.replace('#', '')}><path d={wavePath} /></g>
                 <clipPath id={clipPathId}>
                   <path d={wavePath} />
                 </clipPath>
+                <linearGradient
+                  id="egg-fill-gradient"
+                  x1="0%"
+                  y1="0%"
+                  x2="100%"
+                  y2="100%"
+                >
+                  <stop offset="0%" stopColor="#ffff07" />
+                  <stop offset="100%" stopColor="#ff007f" />
+                </linearGradient>
               </defs>
               <image
-                href={eggFillSrc}
+                href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 40'><defs>${EGG_FILL_GRADIENT}</defs><rect width='30' height='40' fill='url(%23egg-fill-gradient)' /></svg>"
                 clipPath={`url(#${clipPathId})`}
                 width={SVG_WIDTH}
                 height={SVG_HEIGHT}
@@ -174,12 +214,12 @@ export function LoadingOverlay({ status }: LoadingOverlayProps) {
               />
             </svg>
           </div>
-          <img
-            src={eggShellSrc}
-            alt=""
+          <svg
             className="loading-overlay__egg-shell"
-            loading="eager"
+            viewBox={`0 0 ${SVG_WIDTH} ${SVG_HEIGHT}`}
+            role="presentation"
             aria-hidden="true"
+            dangerouslySetInnerHTML={{ __html: EGG_SHELL_SVG }}
           />
         </div>
         <div className="loading-overlay__status">{status.message}</div>
