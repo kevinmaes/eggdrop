eba44dc606833213c39de0b01980583ee25c57ea
Kevin Maes <kevin@kevinmaes.com>
2025-11-10 11:36:23 +0100

    fix: restore loader fill render with inline svg

--- src/LoadingOverlay/LoadingOverlay.tsx
+++ src/LoadingOverlay/LoadingOverlay.tsx
@@ -1,25 +1,7 @@
 import { useEffect, useId, useMemo, useState } from 'react';
 
-const EGG_SHELL_SVG = `
-<svg width="120" height="160" viewBox="0 0 30 40" xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <radialGradient id="egg-shell-gradient" cx="12" cy="15" r="53" fx="12" fy="15" gradientUnits="userSpaceOnUse">
-      <stop offset="0%" stop-color="#ffffff" stop-opacity="1"></stop>
-      <stop offset="100%" stop-color="#ff007f" stop-opacity="1"></stop>
-    </radialGradient>
-  </defs>
-  <g transform="scale(1,-1) translate(0,-40)">
-    <path d="M 11.572032 30.626894 C 13.195086 30.626894 14.472609 30.186552 15.768286 29.488800 C 18.873883 27.816370 21.719542 22.041264 22.557304 18.689818 C 22.807578 17.688634 23.581721 14.210572 23.300863 13.367922 C 23.223213 13.134928 22.881550 12.909206 22.693091 12.585482 C 22.862431 12.698388 22.963490 12.868238 23.093963 12.824738 C 23.532571 12.678523 23.015411 9.847272 22.867662 9.403989 C 22.043338 6.930727 20.948618 4.964925 18.897709 3.286715 C 15.878748 0.816385 10.975051 0.235203 7.518021 2.096894 C 6.826283 2.469405 6.089278 2.848610 5.474844 3.351380 C 5.154935 3.613158 4.872470 4.069692 4.395070 4.308418 C 3.388376 5.270205 2.678400 6.493044 2.170864 7.677440 C -0.134425 13.057038 1.238891 19.134327 3.910140 24.095774 C 4.788476 25.727153 5.879988 27.430077 7.336978 28.622293 C 8.353246 29.453878 9.410161 29.998876 10.653901 30.413498 C 10.895615 30.494083 11.176848 30.468803 11.572032 30.626894 Z" fill="url(#egg-shell-gradient)" transform="translate(2.8,-0.8)"></path>
-  </g>
-</svg>
-`;
-
-const EGG_FILL_GRADIENT = `
-<linearGradient id="egg-fill-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-  <stop offset="0%" stop-color="#ffff07"></stop>
-  <stop offset="100%" stop-color="#ff007f"></stop>
-</linearGradient>
-`;
+const EGG_SHELL_PATH =
+  'M 11.572032 30.626894 C 13.195086 30.626894 14.472609 30.186552 15.768286 29.488800 C 18.873883 27.816370 21.719542 22.041264 22.557304 18.689818 C 22.807578 17.688634 23.581721 14.210572 23.300863 13.367922 C 23.223213 13.134928 22.881550 12.909206 22.693091 12.585482 C 22.862431 12.698388 22.963490 12.868238 23.093963 12.824738 C 23.532571 12.678523 23.015411 9.847272 22.867662 9.403989 C 22.043338 6.930727 20.948618 4.964925 18.897709 3.286715 C 15.878748 0.816385 10.975051 0.235203 7.518021 2.096894 C 6.826283 2.469405 6.089278 2.848610 5.474844 3.351380 C 5.154935 3.613158 4.872470 4.069692 4.395070 4.308418 C 3.388376 5.270205 2.678400 6.493044 2.170864 7.677440 C -0.134425 13.057038 1.238891 19.134327 3.910140 24.095774 C 4.788476 25.727153 5.879988 27.430077 7.336978 28.622293 C 8.353246 29.453878 9.410161 29.998876 10.653901 30.413498 C 10.895615 30.494083 11.176848 30.468803 11.572032 30.626894 Z';
 
 import type { LoadingStatus } from '../Loading/loading.machine';
 import './LoadingOverlay.css';
@@ -67,7 +49,14 @@ export function LoadingOverlay({ status }: LoadingOverlayProps) {
     Math.min(100, Math.round(status.progress * 100))
   );
   const prefersReducedMotion = usePrefersReducedMotion();
-  const clipPathId = useId();
+  const rawId = useId();
+  const idBase = useMemo(
+    () => rawId.replace(/[^a-zA-Z0-9_-]/g, '') || 'loader',
+    [rawId]
+  );
+  const waveClipId = `${idBase}-wave-clip`;
+  const fillGradientId = `${idBase}-fill-gradient`;
+  const shellGradientId = `${idBase}-shell-gradient`;
   const targetProgress = clampProgress(status.progress);
   const [wavePhase, setWavePhase] = useState(0);
   const [animatedProgress, setAnimatedProgress] = useState(targetProgress);
@@ -179,23 +168,11 @@ export function LoadingOverlay({ status }: LoadingOverlayProps) {
               }}
             >
               <defs>
-                <g id="egg-shell-vector">
-                  <svg
-                    width="120"
-                    height="160"
-                    viewBox="0 0 30 40"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <defs dangerouslySetInnerHTML={{ __html: EGG_FILL_GRADIENT }} />
-                  </svg>
-                </g>
-                <rect id="egg-fill-rect" width={SVG_WIDTH} height={SVG_HEIGHT} />
-                <g id={clipPathId.replace('#', '')}><path d={wavePath} /></g>
-                <clipPath id={clipPathId}>
+                <clipPath id={waveClipId}>
                   <path d={wavePath} />
                 </clipPath>
                 <linearGradient
-                  id="egg-fill-gradient"
+                  id={fillGradientId}
                   x1="0%"
                   y1="0%"
                   x2="100%"
@@ -205,22 +182,44 @@ export function LoadingOverlay({ status }: LoadingOverlayProps) {
                   <stop offset="100%" stopColor="#ff007f" />
                 </linearGradient>
               </defs>
-              <image
-                href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 40'><defs>${EGG_FILL_GRADIENT}</defs><rect width='30' height='40' fill='url(%23egg-fill-gradient)' /></svg>"
-                clipPath={`url(#${clipPathId})`}
+              <rect
                 width={SVG_WIDTH}
                 height={SVG_HEIGHT}
-                preserveAspectRatio="xMidYMid slice"
+                fill={`url(#${fillGradientId})`}
+                clipPath={`url(#${waveClipId})`}
               />
             </svg>
           </div>
           <svg
             className="loading-overlay__egg-shell"
-            viewBox={`0 0 ${SVG_WIDTH} ${SVG_HEIGHT}`}
+            viewBox="0 0 30 40"
             role="presentation"
             aria-hidden="true"
-            dangerouslySetInnerHTML={{ __html: EGG_SHELL_SVG }}
-          />
+            width={SVG_WIDTH}
+            height={SVG_HEIGHT}
+          >
+            <defs>
+              <radialGradient
+                id={shellGradientId}
+                cx="12"
+                cy="15"
+                r="53"
+                fx="12"
+                fy="15"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0%" stopColor="#ffffff" stopOpacity="1" />
+                <stop offset="100%" stopColor="#ff007f" stopOpacity="1" />
+              </radialGradient>
+            </defs>
+            <g transform="scale(1,-1) translate(0,-40)">
+              <path
+                d={EGG_SHELL_PATH}
+                transform="translate(2.8,-0.8)"
+                fill={`url(#${shellGradientId})`}
+              />
+            </g>
+          </svg>
         </div>
         <div className="loading-overlay__status">{status.message}</div>
         <div className="loading-overlay__percentage">{percentage}%</div>
