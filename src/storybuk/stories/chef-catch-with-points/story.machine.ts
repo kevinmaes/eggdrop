e480b12324ee0b5359a94b59bdfa5ea1aca779f3
Kevin Maes <kevin@kevinmaes.com>
2025-12-01 10:19:25 +0100

    Fix import order

--- src/storybuk/stories/chef-catch-with-points/story.machine.ts
+++ src/storybuk/stories/chef-catch-with-points/story.machine.ts
@@ -4,12 +4,12 @@ import { nanoid } from 'nanoid';
 import { assign, sendTo, setup, type ActorRefFrom } from 'xstate';
 
 import { eggCaughtPointsMachine } from '../../../EggCaughtPoints/eggCaughtPoints.machine';
-import type { EggColor } from '../../../Egg/egg.machine';
 
 import { chefMachine } from './chef.machine';
 import { eggMachine } from './egg.machine';
 import { henMachine } from './hen.machine';
 
+import type { EggColor } from '../../../Egg/egg.machine';
 import type { Position } from '../../../types';
 import type Konva from 'konva';
 
