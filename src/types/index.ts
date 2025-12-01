e2fc14eb9c1cef973536d70a0c1bc867781a4d00
Kevin Maes <kevin@kevinmaes.com>
2025-12-01 09:34:08 +0100

    Fix sound loading warning (#170)
    * Convert sounds to be statically loaded
    
    * Add changeset

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,8 @@ import type Konva from 'konva';
 
 // Utility type: Require at least one property from T
 export type RequireAtLeastOne<T> = {
-  [K in keyof T]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<keyof T, K>>>;
+  [K in keyof T]-?: Required<Pick<T, K>> &
+    Partial<Pick<T, Exclude<keyof T, K>>>;
 }[keyof T];
 
 export interface Position {
