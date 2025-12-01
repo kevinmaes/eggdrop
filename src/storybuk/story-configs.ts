c486451cda89b73a32b9b21c0a79a6d62eebd09a
Kevin Maes <kevin@kevinmaes.com>
2025-12-01 13:11:54 +0100

    Reorder imports

--- src/storybuk/story-configs.ts
+++ src/storybuk/story-configs.ts
@@ -8,13 +8,13 @@ import { storyConfig as chefWithPausesConfig } from './stories/chef-with-pauses/
 import { storyConfig as eggCaughtPointsConfig } from './stories/egg-caught-points-demo/story-config';
 import { storyConfig as eggFallLandOnlyConfig } from './stories/egg-fall-land-only/story-config';
 import { storyConfig as eggFallingConfig } from './stories/egg-falling/story-config';
+import { storyConfig as eggFallingAndBreakingConfig } from './stories/egg-falling-and-breaking/story-config';
 import { storyConfig as eggFallingRotatingConfig } from './stories/egg-falling-rotating/story-config';
 import { storyConfig as eggHatchGameAccurateConfig } from './stories/egg-hatch-game-accurate/story-config';
 import { storyConfig as eggHatchJumpOnlyConfig } from './stories/egg-hatch-jump-only/story-config';
 import { storyConfig as eggIdleConfig } from './stories/egg-idle/story-config';
 import { storyConfig as eggLandHatchConfig } from './stories/egg-land-hatch/story-config';
 import { storyConfig as eggLandHatchExitConfig } from './stories/egg-land-hatch-exit/story-config';
-import { storyConfig as eggFallingAndBreakingConfig } from './stories/egg-falling-and-breaking/story-config';
 import { storyConfig as hatchedStandExitConfig } from './stories/hatched-stand-exit/story-config';
 import { storyConfig as henBackAndForthConfig } from './stories/hen-back-and-forth/story-config';
 import { storyConfig as henChefCatchConfig } from './stories/hen-chef-catch/story-config';
